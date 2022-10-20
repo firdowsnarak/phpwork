af6e749ca6a4746fc3d79e947f7f5cf27ebb3712
firdowsnarak <s65309010023@ptnicec.ac.th>
2022-10-20 18:33:32 +0700

    ส่ง test ครั้งที่ 1

---
+++ test.php
@@ -0,0 +1,23 @@
+<?php
+
+echo"firdows smaae";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<br> lnformation technology <br>
+<?php
+echo "Pattani";
+?>
+</body>
+</html>
+<?php
+echo "Thailand";
+?>
